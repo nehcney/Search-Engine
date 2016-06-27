aedf1fad90a8d858f7739c5721b7d40c8b42cd5b
Yen Chen <nehcney@gmail.com>
2016-06-27 12:20:43 -0700

    Update SearcherImpl.h

--- SearcherImpl.h
+++ SearcherImpl.h
@@ -1,3 +1,11 @@
+// The SearcherImpl class is used to search through an index (created by the
+// WebCrawlerImpl class and stored in an IndexerImpl object) for webpages that
+// match a set of search terms. The results are stored in a vector of URLs.
+// Search terms assumptions:
+//    - No punctuation
+//    - No duplicate words
+//    - Can be upper or lowercase
+
 #ifndef SEARCHERIMPL_H
 #define SEARCHERIMPL_H
 
